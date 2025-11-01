bde6715dfcd5f2b1c9d36fc6aa761c6f6d2c1c0f
emid1-o <emidiolop5@gmail.com>
2025-11-01 11:39:40 -0300

    coisas

--- main.js
+++ main.js
@@ -1,13 +1,10 @@
 
 const username = "mdio_";
-const apiKey = "566a28cd486504e88a577cdb0b1897e4";
+//nao vou fazer um backend so pra isso, roube essa api se quiser
+const nada = "c0fd96bd4eedb15646e559594a313257";
 
 const lastHeard = document.getElementById("lastHeard");
 const albumImg = document.getElementById("albumImg") 
-
-
-
-
 
 
 
@@ -25,38 +22,17 @@ document.addEventListener('DOMContentLoaded', () => {
         if (musicaDiv) {
             musicaDiv.style.opacity = '1';
         }
-        const namespace = "emidioVisitas";
-    const name = "visitas";
-
-    const registrarVisita = async () => {
     
-      try {
-        
-        const res = await fetch(`https://api.counterapi.dev/v1/${namespace}/${name}/up`);
-        const data = await res.json();
-
-        
-        console.log(`Total de visitas: ${data.count}`);
-      } catch (err) {
-        console.error("Erro ao registrar visita:", err);
-      }
-    };
-
-    registrarVisita();
   
     }, delayEmMs);
 
 });
 
-fetch(`https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=${username}&api_key=${apiKey}&format=json&limit=1`)
+fetch(`https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=${username}&api_key=${nada}&format=json&limit=1`)
   .then(res => res.json())
   .then(data => {
     const track = data.recenttracks.track[0];
     const albumImage = track.image.find(img => img.size === "medium")["#text"];
-    console.log("Última música:", track.name);
-    console.log("Artista:", track.artist["#text"]);
-    console.log("Album:", track.album["#text"]);
-    console.log("Tocando agora?", track["@attr"]?.nowplaying === "true" ? "Sim" : "Não");
     lastHeard.textContent = `Último scrobble: ${track.name} - ${track.artist["#text"]}`;
     albumImg.style.backgroundImage = `url(${albumImage})`;
   })
